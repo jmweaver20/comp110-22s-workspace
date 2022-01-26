3a0319ac18d8b8d4cf77797c4c2efac10e3ea59d
Jonathan Weaver <jmweaver@ad.unc.edu>
2022-01-25 20:38:45 -0500

    descriptions of what certain code does

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -7,13 +7,16 @@ user_guess: str = input("What is your 6-letter guess? ")
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
-i: int = 0  # Index Tracker
+i: int = 0  # Index Tracker for Main Loop
 i2: int = 0  # Index Tracker for Yellow Boxes - make sure we need this?
 char_exist: bool = False
 emoji_guess: str = ""
 
+# This loop ensures that the user's guess word is matching in length
+# to the secret word.
 while (len(user_guess) < len(secret_word) or len(user_guess) > len(secret_word)):
     user_guess = str(input("That was not 6 letters! Try again: "))
+
 
 while (i < len(secret_word)):
 
@@ -22,7 +25,7 @@ while (i < len(secret_word)):
         i = i + 1
     else:
         i = i + 1
-        while (char_exist != True and (i2 < len(secret_word))):  # != operator not valid? why is it underlined
+        while (char_exist != True and (i2 < len(secret_word))):
             if (secret_word[i2] == user_guess[i]):
                 char_exist = True
             else:
@@ -30,7 +33,7 @@ while (i < len(secret_word)):
         if (char_exist):
             emoji_guess = emoji_guess + YELLOW_BOX
         else:
-            emoji_guess = emoji_guess + WHITE_BOX  # why is white box not printing
+            emoji_guess = emoji_guess + WHITE_BOX
 
 print(emoji_guess)
 
@@ -38,3 +41,7 @@ if (user_guess == secret_word):
     print("Woo! You got it!")
 else:
     print("Not quite! Play again soon.")
+
+# Instructions said to use f-string outputs but I haven't found a place to use those?
+# why does the char_exist != True logic not working (it's underlined)
+# white box when char_exist is false isn't printing?
