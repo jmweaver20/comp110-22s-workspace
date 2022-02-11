23036bcaa4443afb0adb40c23fb29b25a03bfbb5
Jonathan Weaver <jmweaver@ad.unc.edu>
2022-02-11 12:04:15 -0500

    function updates with floats for coordinates

--- exercises/ex04_turtle.py
+++ exercises/ex04_turtle.py
@@ -10,15 +10,12 @@ def main() -> None:
     """The entrypoint of my scene."""
     bob: Turtle = Turtle()
     leo: Turtle = Turtle()
-    draw_sun(bob, leo)
+    draw_sun(bob, leo, -200.0, 250.0)
     draw_house_base(bob, leo, 0.0, 0.0)
-
-    # TODO: Declare your Turtle variable(s) here.
-    # TODO: Call the procedures you define and pass your Turtle(s) as an argument.
     done()
 
 
-def draw_sun(sun: Turtle, outline: Turtle) -> None:   # what kind of data type does this return or none?
+def draw_sun(sun: Turtle, outline: Turtle, x: float, y: float) -> None:   # what kind of data type does this return or none?
     """Function that draws the sun for the picture."""
     sun.color(246, 207, 7)
     outline.color(234, 171, 32)
@@ -26,24 +23,24 @@ def draw_sun(sun: Turtle, outline: Turtle) -> None:   # what kind of data type d
     outline.speed(0)
 
     sun.up()
-    sun.goto(-200, 250)
+    sun.goto(x, y)
     sun.down()
     sun.begin_fill()
-    x: int = 0
-    while (x < 100):
+    i: int = 0
+    while (i < 100):
         sun.forward(5)
         sun.right(3.6)
-        x += 1
+        i += 1
     sun.end_fill()
 
     outline.up()
-    outline.goto(-200, 250)
+    outline.goto(x, y)
     outline.down()
-    i: int = 0
-    while (i < 100):
+    i2: int = 0
+    while (i2 < 100):
         outline.forward(5)
         outline.right(3.6)
-        i += 1
+        i2 += 1
 
 
 def draw_house_base(base: Turtle, windows: Turtle, x: float, y: float) -> None:
@@ -56,6 +53,7 @@ def draw_house_base(base: Turtle, windows: Turtle, x: float, y: float) -> None:
     while (i < 4):
         base.forward(250)
         base.right(90)
+        base.forward(25)
         i += 1
     
 
