fd22b0f7d6c3a3a1cff40ae4dd969a656db42f57
Jonathan Weaver <jmweaver@ad.unc.edu>
2022-02-12 16:23:55 -0500

    added draw roof function

--- exercises/ex04_turtle.py
+++ exercises/ex04_turtle.py
@@ -3,6 +3,7 @@
 __author__ = "730397253"
 
 from turtle import Turtle, colormode, done 
+from random import randint
 colormode(255)
 
 
@@ -12,10 +13,11 @@ def main() -> None:
     leo: Turtle = Turtle()
     draw_sun(bob, leo, -200.0, 250.0)
     draw_house_base(bob, leo, 0.0, 0.0)
+    draw_house_roof(bob, 10, 62, 270)
     done()
 
 
-def draw_sun(sun: Turtle, outline: Turtle, x: float, y: float) -> None:   # what kind of data type does this return or none?
+def draw_sun(sun: Turtle, outline: Turtle, x: float, y: float) -> None:
     """Function that draws the sun for the picture."""
     sun.color(246, 207, 7)
     outline.color(234, 171, 32)
@@ -28,7 +30,7 @@ def draw_sun(sun: Turtle, outline: Turtle, x: float, y: float) -> None:   # what
     sun.begin_fill()
     i: int = 0
     while (i < 100):
-        sun.forward(5)
+        sun.forward(2)
         sun.right(3.6)
         i += 1
     sun.end_fill()
@@ -38,7 +40,7 @@ def draw_sun(sun: Turtle, outline: Turtle, x: float, y: float) -> None:   # what
     outline.down()
     i2: int = 0
     while (i2 < 100):
-        outline.forward(5)
+        outline.forward(2)
         outline.right(3.6)
         i2 += 1
 
@@ -55,22 +57,47 @@ def draw_house_base(base: Turtle, windows: Turtle, x: float, y: float) -> None:
         base.right(90)
         base.forward(25)
         i += 1
-    make_square(windows, x + 5, y + -45)
-    make_square(windows, x + 175, y + -45)
+    make_square(windows, x + 5, y + -45, 50)
+    make_square(windows, x + 175, y + -45, 50)
 
 
-def make_square(square: Turtle, x: float, y: float) -> None:
-    "Draws a square at desired x, y position."
+def draw_house_roof(roof: Turtle, x: float, y: float, length: float) -> None:
+    """Draws the roof of the house, adds a 'shadow' to the roof based on a randint number - attempting fun
+    grading category."""
+    make_triangle(roof, x, y, length)
+    i: int = 0
+    leng: float = length
+    while (i < randint(20, 75)):
+        leng = leng * 0.97
+        make_triangle(roof, x, y, leng)
+        i += 1
+
+
+def make_square(square: Turtle, x: float, y: float, width: float) -> None:
+    "Draws a square at desired x, y position and desired width."
     square.color(0, 0, 0)
     square.up()
     square.goto(x, y)
     square.down()
     i: int = 0
     while (i < 4):
-        square.forward(50)
+        square.forward(width)
         square.right(90)
         i += 1
     
+
+def make_triangle(tri: Turtle, x: float, y: float, length: float) -> None:
+    """Draws a triangle at desired x, y position and desired length."""
+    tri.color(0, 0, 0)
+    tri.up()
+    tri.goto(x, y)
+    tri.down()
+    i: int = 0
+    while (i < 3):
+        tri.forward(length)
+        tri.left(120)
+        i += 1
+
 
 if __name__ == "__main__":
     main()
