8e57a587186b18aaee76290e1ef7379b1556c53a
Jonathan Weaver <jmweaver@ad.unc.edu>
2022-02-25 15:41:41 -0500

    begin writing test code for dictionaries file

--- exercises/ex06/dictionary_test.py
+++ exercises/ex06/dictionary_test.py
@@ -1,3 +1,26 @@
 """Test file for the dictionary file (ex06)."""
 
 __author__ = "730397253"
+
+from dictionary import invert, favorite_color, count
+import pytest
+
+
+def test_invert_order() -> None:
+    """Ensures that invert function inverts the input parameters. Use Case."""
+    test_val: dict[str, str] = {'a': 'b', 'c': 'd', 'e': 'f'}
+    assert invert(test_val) == {'b': 'a', 'd': 'c', 'f': 'e'}
+
+
+def test_invert_one() -> None:
+    """Ensures that if the dict is one value, it is inverted. Use Case."""
+    test_val: dict[str, str] = {'a': 'b'}
+    assert invert(test_val) == {'b': 'a'}
+
+
+def test_invert_keyerror() -> None:
+    """Ensures that function raises error if duplicate keys. Edge Case."""
+    with pytest.raises(KeyError):
+        test_val: dict[str, str] = {'a': 'b', 'c': 'b'}
+        invert(test_val)
+
