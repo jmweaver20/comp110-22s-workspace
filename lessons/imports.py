28a9a720df9a953e745feeedd070aad5f91bcd25
Jonathan Weaver <jmweaver@ad.unc.edu>
2022-03-01 10:25:10 -0500

    naming updates and helper functions and importing

---
+++ lessons/imports.py
@@ -0,0 +1,13 @@
+"""Examples of importing in python."""
+
+
+from lessons import helpers
+
+
+def main() -> None:
+    """Entrypoint of our program."""
+    print(helpers.powerful(2, 4))
+
+
+if __name__ == "__main__":
+    main()
