4eccfd5a6e456a43b9776779bfef3947026f9ce7
Jonathan Weaver <jmweaver@ad.unc.edu>
2022-03-01 10:58:26 -0500

    commit updates

--- lessons/helpers.py
+++ lessons/helpers.py
@@ -1,6 +1,20 @@
 """Demonstrating defining a module imported elsewhere."""
 
 
+def main() -> None:
+    print(powerful(2, 10))
+    print("helpers.py run as a module")
+
+
 def powerful(x: float, n: float) -> float:
     """Raise x to the power of n."""
     return x ** n
+
+
+# Idiom for making a module able to run as a program
+# or have its global definitions imported elsewhere.
+if __name__ == "__main__":
+    main()
+else:
+    # Not normal to have an else branch in this if statement.
+    print(f"helpers.py was imported: {__name__}")
--- lessons/imports.py
+++ lessons/imports.py
@@ -1,12 +1,14 @@
 """Examples of importing in python."""
 
 
-from lessons import helpers
+# Allows us to use helpers as hp instead of writing
+# out of all helpers
+from lessons import helpers as hp
 
 
 def main() -> None:
     """Entrypoint of our program."""
-    print(helpers.powerful(2, 4))
+    print(hp.powerful(2, 4))
 
 
 if __name__ == "__main__":
