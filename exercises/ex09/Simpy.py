b918c75f90d9e1f0c83899e5022aee9013060b8c
Jonathan Weaver <jmweaver@ad.unc.edu>
2022-03-31 11:59:15 -0400

    simpy progress

--- exercises/ex09/Simpy.py
+++ exercises/ex09/Simpy.py
@@ -4,10 +4,34 @@ from __future__ import annotations
 
 from typing import Union
 
-__author__ = "YOUR PID HERE"
+from numpy import str0
+
+__author__ = "730397253"
 
 
 class Simpy:
     values: list[float]
 
-    # TODO: Your constructor and methods will go here.
+    def __init__(self, vals: list[float]):
+        """Takes parameter of float values and initializes to a new Simpy object."""
+        self.values = vals
+    
+    def __str__(self) -> str:
+        """Returns string representation of simpy."""
+        return f"Simpy({self.values})"
+    
+    def fill(self, filler: float, num: int) -> None:
+        """Fills a simpy's values with filler values num amount of times."""
+        i: int = 0
+        while (i < num):
+            self.values.append(filler)
+            i += 1
+    
+    def arrange(self, start: float, stop: float, step: float) -> Simpy:
+        """Fills the values attribute with a range of values in terms of floats."""
+        assert step != 0.0
+        filled: Simpy = Simpy([])
+        
+
+
+
