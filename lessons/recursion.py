1658483e5048bb817a876ec366f7e128ff1b5c7d
Jonathan Weaver <jmweaver@ad.unc.edu>
2022-04-19 11:15:05 -0400

    intro to recursion and recursive methods

---
+++ lessons/recursion.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from typing import Union
+
+
+class Node:
+    data: int
+    next: Union[Node, None]
+
+    def __init__(self, data: int, next: Union[Node, None]):
+        self.data = data
+        self.next = next
+    
+    def __str__(self) -> str:
+        if self.next is None:
+            return f"{self.data} -> None"
+        else:
+            return f"{self.data} -> {self.next}"
+
+
+def sum(node: Node) -> int:
+    if node.next is None:
+        return node.data
+    else:
+        return node.data + sum(node.next)
+
+
+def count(node: Node, current_count: int = 0) -> int:
+    if node.next is None:
+        return current_count + 1
+    else:
+        return count(node.next, current_count + 1)
+    
+
+head: Node = Node(3, None)
+head = Node(2, head)
+head = Node(1, head)
+print(sum(head))
+print(count(head))
+print(head)
